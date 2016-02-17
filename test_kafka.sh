1a88b6215a1cc64816619ac512d1db31156222f5
root <root@kafka1.(none)>
2016-02-17 10:45:18 +0900

    just modified

--- test_kafka.sh
+++ test_kafka.sh
@@ -1,6 +1,11 @@
 . /usr/src/env_conf
 
 test_type=$1
+consumer_group=$2
+if [ -z $consumer_group ]
+then
+ consumer_group=test_group
+fi
 
 if [ $test_type = "describe" ]
 then
@@ -10,5 +15,5 @@ then
  $KAFKA_HOME/bin/kafka-console-producer.sh --broker-list $broker_list --topic $kafka_topic
 elif [ $test_type = "consumer" ]
 then
- $KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper $zookeeper_ip --topic $kafka_topic --from-beginning
+ $KAFKA_HOME/bin/kafka-console-consumer.sh --zookeeper $zookeeper_ip --topic $kafka_topic --group $consumer_group
 fi

54bae6ab1bb7c39b761382732997b274b9b289ff
System Administrator <root@mi-houi-MacBook-Air.local>
2016-10-28 10:04:48 +0900

    modified

--- watch_agent_install.sh
+++ watch_agent_install.sh
@@ -1,7 +1,7 @@
 api_key=$1
 secret_key=$2
 
-cd /opt/;wget https://github.com/starwinds/watchagent/blob/master/ucloud_watch_agent-1.0.0-1.x86_64.rpm
+cd /opt/;wget https://raw.githubusercontent.com/starwinds/watchagent/master/ucloud_watch_agent-1.0.0-1.x86_64.rpm 
 rpm -ivh ucloud_watch_agent-1.0.0-1.x86_64.rpm
 cd /watch/agent/scripts/
 /bin/bash postinst.sh $api_key $secret_key
